97ad0e9bfbdb870f5273d09eda119407e4643a71
Facundo <facugastonbarral@gmail.com>
2022-11-25 02:47:29 -0300

    Add support for multiple accounts

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ load_dotenv()
 class Bot(commands.Bot):
     def __init__(self):
         self.partidas = []
-        super().__init__(os.environ.get('TOKEN'), prefix='?', initial_channels=[os.environ.get('CHANNEL')])
+        self._uuidList = json.loads(os.environ.get("UUID"))
+        self._channelList = json.loads(os.environ.get("CHANNEL"))
+        super().__init__(os.environ.get('TOKEN'), prefix='?', initial_channels=self._channelList)
 
     def api_get_json(self, url):
         try:            
@@ -49,38 +51,43 @@ class Bot(commands.Bot):
         @routines.routine(seconds=int(os.environ.get('TIMER')))
         async def partidas(self):
             print("» Waiting «")
-
-            # Get teams of player 
-            teams = self.api_get_json(f'https://www.checkmategaming.com/api/core/teamsForMember/{os.environ.get("UUID")}/null/null/null')
-            if (not teams): return;
-
-            # Search for matches on each team
             self.matchesCount += 1 # Counter to prevent announce already started matchs before bot is ready
-            for i in range(len(teams['teams'])):
-                currentTeam = teams['teams'][i]['id']
+            for channelIndex, uuid in enumerate(self._uuidList):
+                # Get teams of player 
+                teams = self.api_get_json(f'https://www.checkmategaming.com/api/core/teamsForMember/{uuid}/null/null/null')
+                if (not teams): return;
 
-                # Get matchs of current team
-                matchs = self.api_get_json(f'https://www.checkmategaming.com/api/core/team/{currentTeam}/match-list?team_id={currentTeam}&offset=0&limit=3')
-                if (not matchs or not 'data' in matchs or not 'ladders' in matchs['data'] or matchs['data']['ladders'] == None): continue;
+                # Search for matches on each team
+                for i in range(len(teams['teams'])):
+                    currentTeam = teams['teams'][i]['id']
+
+                    # Get matchs of current team
+                    matchs = self.api_get_json(f'https://www.checkmategaming.com/api/core/team/{currentTeam}/match-list?team_id={currentTeam}&offset=0&limit=3')
+                    if (not matchs or not 'data' in matchs or not 'ladders' in matchs['data'] or matchs['data']['ladders'] == None): continue;
+                    
+                    # Check if have scheduled matchs on response
+                    scheduled = 'scheduled' in matchs['data']['ladders'][0]['matches']
+            
+                    # Handle data of scheduled match
+                    if scheduled: 
+                        for j in range(len((matchs['data']['ladders'][0]['matches']['scheduled']))):
+                            # Get match data
+                            match = (matchs['data']['ladders'][0]['matches']['scheduled'][j])
+                            matchId = match['id']
+                            if ((matchId in self.partidas) or match['match_status'] != "Scheduled"): continue; # Continue if match was already announced or status isnt Scheduled
+                            mode = teams['teams'][i]["isSingleType"] and "1v1" or "2v2" # Get match mode
+                            self.partidas.append(matchId)
+                            print("» New match «")
+                            # if self.matchesCount == 1: continue; # Not announce match on first iteration
+                            matchData = self.getMatchData("https://www.checkmategaming.com/es" + match['match_url'])
+                            channel = self.connected_channels[channelIndex]
+                            print(channel)
+                            print(f"Nueva partida contra {match['opponent_team_name']} por {matchData['pot']} de POT [{mode}] [BO{matchData['bo']}] https://www.checkmategaming.com/es/matchfinder-ladder-500-challenge-{matchId}-match-details")
+                            await channel.send(f"Nueva partida contra {match['opponent_team_name']} por {matchData['pot']} de POT [{mode}] [BO{matchData['bo']}] https://www.checkmategaming.com/es/matchfinder-ladder-500-challenge-{matchId}-match-details")
                 
-                # Check if have scheduled matchs on response
-                scheduled = 'scheduled' in matchs['data']['ladders'][0]['matches']
-        
-                # Handle data of scheduled match
-                if scheduled: 
-                    for j in range(len((matchs['data']['ladders'][0]['matches']['scheduled']))):
-                        # Get match data
-                        match = (matchs['data']['ladders'][0]['matches']['scheduled'][j])
-                        matchId = match['id']
-                        if ((matchId in self.partidas) or match['match_status'] != "Scheduled"): continue; # Continue if match was already announced or status isnt Scheduled
-                        mode = teams['teams'][i]["isSingleType"] and "1v1" or "2v2" # Get match mode
-                        self.partidas.append(matchId)
-                        print("» New match «")
-                        if self.matchesCount == 1: continue; # Not announce match on first iteration
-                        matchData = self.getMatchData("https://www.checkmategaming.com/es" + match['match_url'])
-                        print(f"Nueva partida contra {match['opponent_team_name']} por {matchData['pot']} de POT [{mode}] [BO{matchData['bo']}] https://www.checkmategaming.com/es/matchfinder-ladder-500-challenge-{matchId}-match-details")
-                        await self.mainChannel.send(f"Nueva partida contra {match['opponent_team_name']} por {matchData['pot']} de POT [{mode}] [BO{matchData['bo']}] https://www.checkmategaming.com/es/matchfinder-ladder-500-challenge-{matchId}-match-details")
         partidas.start(self)
 
 bot = Bot()
 bot.run()
+
+
